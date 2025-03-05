e316fadba84cbbc91437068cbddea2595e519533
khenvalencia <valenciakhen522@gmail.com>
2025-03-05 11:58:29 +0800

    Update SearchBar.js

--- SearchBar.js
+++ SearchBar.js
@@ -1,1 +1,25 @@
+import React from "react";
+
+const SearchBar = ({ searchQuery, handleSearch }) => {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchQuery}
+        onChange={handleSearch}
+        style={{
+          padding: "10px",
+          width: "100%",
+          maxWidth: "400px",
+          fontSize: "16px",
+          borderRadius: "8px",
+          border: "1px solid #ccc",
+        }}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
 
