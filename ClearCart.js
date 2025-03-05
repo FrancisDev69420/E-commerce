3e54eb40ec5d12f45293d94407ef9eb5ccf79c5c
khenvalencia <valenciakhen522@gmail.com>
2025-03-05 12:08:21 +0800

    Update ClearCart.js

--- ClearCart.js
+++ ClearCart.js
@@ -1,1 +1,26 @@
+import React from "react";
+
+const ClearCart = ({ clearCart }) => {
+  return (
+    <div>
+      <button onClick={clearCart} style={styles.button}>
+        Clear Cart
+      </button>
+    </div>
+  );
+};
+
+const styles = {
+  button: {
+    padding: "10px 20px",
+    backgroundColor: "#f44336",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px",
+  },
+};
+
+export default ClearCart;
 
